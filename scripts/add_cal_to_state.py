b40459ac7261b2455d33e6e9cbe3b9448b348ec3
mmemoo <mehmet.culcu2010@gmail.com>
2025-07-31 15:08:22 +0300

    create add_cal_to_state func

---
+++ scripts/add_cal_to_state.py
@@ -0,0 +1,15 @@
+import json
+from check_state import check_and_update_state
+
+def add_cal_to_state(cals):
+    check_and_update_state()
+    
+    with open("state.json","r") as f:
+        state_json = json.load(f)
+
+    state_json["total_cals"] += cals
+
+    with open("state.json","w") as f:
+        json.dump(state_json,f)
+
+add_cal_to_state(1)
