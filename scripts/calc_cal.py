4d381b92750802ae672f678a8179e7773790efa8
mmemoo <mehmet.culcu2010@gmail.com>
2025-08-01 18:39:42 +0300

    fixed paths

--- scripts/calc_cal.py
+++ scripts/calc_cal.py
@@ -11,7 +11,7 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 encoder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-food_csv = pandas.read_csv("usda_data/food.csv")
+food_csv = pandas.read_csv("data/usda_data/food.csv")
 food_csv = food_csv[[type(desc) == str for desc in food_csv["description"]]]
 food_csv["description"] = [i.lower().replace(",","") for i in food_csv["description"].tolist()]
 
